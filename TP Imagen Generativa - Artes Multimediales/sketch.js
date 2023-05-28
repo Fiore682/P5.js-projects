e0830320cf431d6c1fbeee828948c975c728b268
Fiorella Medina <fiorella682@hotmail.com>
2023-05-28 01:31:15 -0300

    update de imagen generativa

--- TP Imagen Generativa - Artes Multimediales/sketch.js
+++ TP Imagen Generativa - Artes Multimediales/sketch.js
@@ -25,30 +25,63 @@ function draw() {
 
     //lineas horizontales superiores
     push();
-    var lineSpacing = 300 / 150;
-    var lineWidth = 1;
+    if (keyIsPressed) {
+        var lineSpacing = 600 / 380;
+        var lineWidth = 0.15;
 
-    for (let i = 0; i < 50; i++) {
-        var y = lineSpacing * (i + 10);
-        drawLine(0, y, width, y, lineWidth);
-    }
+        for (let i = 0; i < 80; i++) {
+            var y = lineSpacing * (i + 10);
+            drawLine(0, y, width, y, lineWidth);
+        }
 
-    function drawLine(x1, y1, x2, y2, lineWidth) {
-        stroke(0);
-        strokeWeight(lineWidth);
-        line(x1, y1, x2, y2);
+        function drawLine(x1, y1, x2, y2, lineWidth) {
+            stroke(0);
+            strokeWeight(lineWidth);
+            line(x1, y1, x2, y2);
+        }
+    } else {
+        var lineSpacing = 600 / 380;
+        var lineWidth = 0.5;
+
+        for (let i = 0; i < 80; i++) {
+            var y = lineSpacing * (i + 10);
+            drawLine(0, y, width, y, lineWidth);
+        }
+
+        function drawLine(x1, y1, x2, y2, lineWidth) {
+            stroke(0);
+            strokeWeight(lineWidth);
+            line(x1, y1, x2, y2);
+        }
     }
     pop();
 
-    //Si se mantiene el mouse clickeado aparecen 3 diferentes primitivas
+    //Si se mantiene el mouse clickeado cambia el color de las 3 primitivas
     push();
     var shapeType = floor(random(3));
     var x = random(width);
     var y = random(height / 4);
 
-    noStroke();
-    fill(255, 255, 255);
+
     if (mouseIsPressed) {
+        noStroke();
+        fill(100, 100, 100);
+
+        switch (shapeType) {
+            case 0:
+                ellipse(x, y, 20, 20);
+                break;
+            case 1:
+                rect(x, y, 50, 50);
+                break;
+            case 2:
+                triangle(x, y, x + 50, y, x + 30, y + 30);
+                break;
+        }
+    } else {
+        noStroke();
+        fill(255, 255, 255);
+
         switch (shapeType) {
             case 0:
                 ellipse(x, y, 20, 20);
